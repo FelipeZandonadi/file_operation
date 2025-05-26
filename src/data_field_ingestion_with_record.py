e6672ff288bde3b87ffc9aa5221d1729befc557c
Felipe Zandonadi <felipe.santos@db1.com.br>
2025-05-25 21:35:37 -0300

    Add ingestion with record

--- src/data_field_ingestion_with_record.py
+++ src/data_field_ingestion_with_record.py
@@ -4,13 +4,13 @@ def add_value_to_file(value, file):
     len_value = len(value) + 1
     # +1 for the '|' character
     bin_len_value = len_value.to_bytes(2)
-    file.write(bin_len_value+value.encode('utf-8')+b'|')
+    file.write(bin_len_value+value.encode('utf-8'))
 
 def main():
-    with open('src/output/global_output.txt', 'wb') as file:
-        users = mockup_user_generator(500)
+    with open('output/global_output.txt', 'wb') as file:
+        users = mockup_user_generator(3)
         for user in users:
-            add_value_to_file(f'{user['first_name']}|{user['last_name']}|{user['street_name']}|{user['street_number']}|{user['city']}|{user['state']}|{user['postcode']}', file)
+            add_value_to_file(f'{user['first_name']}|{user['last_name']}|{user['street_name']}|{user['street_number']}|{user['city']}|{user['state']}|{user['postcode']}|', file)
 
 if __name__ == "__main__":
     main()
